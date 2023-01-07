66abf5fcf69ce4c39ba2bab7bc3a1d4faa2df271
Mohammad amin Jafarian <amin.jafarian82@gmail.com>
2023-01-07 21:51:36 +0330

    byte text convertor
    - now auto beautify and convert byte texts to normal strings

--- index.js
+++ index.js
@@ -18,7 +18,31 @@ for (let line of beautified.split("\n")) {
     for (const variable of variables) {
         const name = variable.split(" = ")[0].replace("[", "").replace("]", "")
         const value = variable.split(" = ")[1]
-        Variables[name] = value
+        if (value.includes(`"`) && value.includes(`\\`)) {
+            const finalValue = value.slice(1, -1).split("\\").map(Number).map(x => {
+                const Converted = String.fromCharCode(x)
+                if (Converted.includes(`\\`)) {
+                    return `\\\\`
+                } else if (Converted.includes(`"`)) {
+                    return `\\"`
+                } else if (Converted.includes(`'`)) {
+                    return `\\'`
+                } else if (Converted.includes(`\n`)) {
+                    return `\\n`
+                } else if (Converted.includes(`\r`)) {
+                    return `\\r`
+                } else if (Converted.includes(`\t`)) {
+                    return `\\t`
+                } else if (Converted.includes(`\v`)) {
+                    return `\\v`
+                } else {
+                    return Converted
+                }
+            }).join("").substring(1)
+            Variables[name] = `"${finalValue}"`
+        } else {
+            Variables[name] = value
+        }
     }
    } else if (size_dict(Variables) > 0) {
         while (line.includes("L_1_")) {
@@ -29,6 +53,6 @@ for (let line of beautified.split("\n")) {
    }
 }
 
-const finalCodebeautified = luamin.Beautify(finalCode, {RenameVariables: false, RenameGlobals: false, SolveMath: true}).split("\n").slice(4).join("\n")
-fs.writeFileSync('output.lua', finalCodebeautified, 'utf8');
+const finalCodeBeautified = luamin.Beautify(finalCode, {RenameVariables: false, RenameGlobals: false, SolveMath: true}).split("\n").slice(4).join("\n")
+fs.writeFileSync('output.lua', finalCodeBeautified, 'utf8');
 console.log("Done! cleaned file now saved inside output.lua")
